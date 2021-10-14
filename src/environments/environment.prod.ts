08a9871f2f76b69f0ac262a064cd8f07f6b3ef42
Arturo <arturo_ajsa@hotmail.com>
2021-10-14 10:30:43 -0500

    elimnando archivos malos

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  //production: true
-  //apiUrl: 'http://192.168.100.150/suit/backend/',
-  apiUrl: 'http://192.168.100.150/codigneiter/index.php/',
-  anecdotaUrl: 'http://192.168.100.150/codigneiter/uploads/',
-};
